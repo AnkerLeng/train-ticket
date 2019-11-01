e9307b835e20e769455eade6e9c9efb69cfda2bc
anker <553628212@qq.com>
2019-11-01 12:15:03 +0800

    4-5 使用Memo&Callback Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,47 @@
-import React, { Component, useState, createContext, useContext } from 'react';
+import React, { Component, useState, useMemo, memo, useCallback } from 'react';
 import './App.css';
 
-const CountContext = createContext();
 
-class Foo extends Component {
-  render() {
-    return (
-      <CountContext.Consumer>
-        {
-          count => <h1>{count}</h1>
-        }
-      </CountContext.Consumer>
-    );
-  }
-}
-
-class Bar extends Component {
-  static contextType = CountContext;
-  render() {
-    const count = this.context
-    return (
-      <h1>{count}</h1>
-    );
-  }
-}
-
-function Counter() {
-  const count = useContext(CountContext);
+const Counter = memo(function Counter(props) {
+  console.log('Counter render')
   return (
-    <h1>{count}</h1>
+    <h1 onClick={props.onClick}>{props.count}</h1>
   )
-}
+});
 
 
 function App(props) {
   const [count, setCount] = useState(0);
+  const [clickCount, setClickCount] = useState(0);
+
+  // useMemo 如果不穿第二个参数则每次运行，传空数组运行一次。
+  const double = useMemo(() => {
+    return count * 2;
+  }, [count === 3]);
+
+  // const half = useMemo(() => {
+  //   return double / 4;
+  // }, [double]);
+
+  // const onClick = useMemo(() => {
+  //   console.log('onClick')
+  // }, [])
+
+  const onClick = useCallback(() => {
+    console.log('onClick')
+    // setClickCount(clickCount + 1);
+    setClickCount((clickCount) => clickCount + 1);
+  }, [clickCount]);
+
   return (
 
     <div>
       <button type="button"
         onClick={() => { setCount(count + 1) }}
       >
-        Add ({count})
+        Add ({count}), double:({double})
       </button>
-      <CountContext.Provider value={count}>
-        <Foo></Foo>
-        <Bar></Bar>
-        <Counter></Counter>
-      </CountContext.Provider>
+      <Counter count={double} onClick={onClick}></Counter>
     </div>
   )
 
